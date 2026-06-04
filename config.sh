d369eec7da6248dcbf6aeb0c1c4436d3fee3b1b2
konfuzhon <b8s13.94i@gmail.com>
2026-06-03 21:25:22 -0400

    Added xbps-src setup

--- config.sh
+++ config.sh
@@ -4,6 +4,10 @@
 sudo xbps-install -S
 sudo xbps-install base-devel dbus elogind pipewire firefox feh mpv xorg xinit libX11-devel libXft-devel libXinerama-devel libXrandr-devel nerd-fonts 
 
+# Set up xbps-src
+cd
+git clone https://github.com/void-linux/void-packages.git
+
 # Set up audio
 sudo ln -s /etc/sv/dbus /var/service/
 sudo ln -s /etc/sv/elogind /var/service/
